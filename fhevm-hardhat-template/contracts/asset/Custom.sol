f02e54468b661fd204e6078904d390a21c9bc0c5
Peter <kompaszbence@gmail.com>
2025-02-10 00:12:20 +0200

    feat: custom asset

---
+++ fhevm-hardhat-template/contracts/asset/Custom.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { AuctionWinner } from "../AuctionWinner.sol";
+import { ConfidentialAuction } from "../ConfidentialAuction.sol";
+
+contract Custom {
+    address asset;
+    AuctionWinner winnerNFT;
+    ConfidentialAuction auction;
+
+    constructor(address _winner, address _auction, address myAsset) {
+        asset = myAsset;
+        winnerNFT = AuctionWinner(_winner);
+        auction = ConfidentialAuction(_auction);
+    }
+
+    function claimWin(uint256 tokenId) public payable {
+        // Get bid
+        ConfidentialAuction.DecryptedBid memory bid = auction.getDecryptedBid(tokenId);
+
+        // Pay the amount
+        require(msg.value > bid.totalVisible, "Not enough msg.value");
+
+        // Burn winner NFT
+        winnerNFT.burn(tokenId);
+        bytes memory data = abi.encodeWithSignature("_mint(uint256)", bid.amountVisible);
+
+        // Call the target contract
+        (bool success, bytes memory result) = asset.call(data);
+
+        require(success, "Failed to mint");
+    }
+}
