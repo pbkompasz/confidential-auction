86e04fc7d31884fb7131b928b9fdecdc6fbf690b
Peter <kompaszbence@gmail.com>
2025-02-10 00:12:59 +0200

    feat: nfts

---
+++ fhevm-hardhat-template/contracts/AuctionPosition.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { AuctionFactory } from "./AuctionFactory.sol";
+import { ConfidentialAuction } from "./ConfidentialAuction.sol";
+
+contract AuctionPosition is ERC721 {
+    mapping(uint256 => uint256) _tokenIds;
+    address factoryAddress;
+    address winnerAddress;
+
+    constructor(address _factoryAddress, address _winnerAddress) ERC721("AuctionHouse", "ACT-P") {
+        factoryAddress = _factoryAddress;
+        winnerAddress = _winnerAddress;
+    }
+
+    // TODO tokenURI
+    function createBidPosition(address user, uint256 auctionId) public returns (uint256) {
+        ConfidentialAuction auctionOwner = AuctionFactory(factoryAddress).getAuction(auctionId);
+        require(address(auctionOwner) == msg.sender, "Only auction can create bid positions.");
+        uint256 newItemId = _tokenIds[auctionId]++;
+        _mint(user, newItemId);
+
+        return newItemId;
+    }
+
+    function burn(uint256 tokenId) external {
+        require(winnerAddress == msg.sender, "Caller is not a burner or owner");
+        _burn(tokenId);
+    }
+}
---
+++ fhevm-hardhat-template/contracts/AuctionWinner.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { AuctionFactory } from "./AuctionFactory.sol";
+import { ConfidentialAuction } from "./ConfidentialAuction.sol";
+
+contract AuctionWinner is Ownable, ERC721 {
+    mapping(uint256 => bool) _tokenIds;
+    address private factoryAddress;
+    address private assetAddress;
+
+    constructor(address _factoryAddress, address _assetAddress) Ownable(msg.sender) ERC721("AuctionWinner", "ACT-W") {
+        factoryAddress = _factoryAddress;
+        assetAddress = _assetAddress;
+    }
+
+    // TODO tokenURI
+    function createWinner(address user, uint256 auctionId) public {
+        ConfidentialAuction auctionOwner = AuctionFactory(factoryAddress).getAuction(auctionId);
+        require(address(auctionOwner) == msg.sender, "Only auction can create bid positions.");
+        require(_tokenIds[auctionId] == false, "Win already claimed.");
+        _mint(user, auctionId);
+        // TODO burn position nft
+    }
+
+    function burn(uint256 tokenId) external {
+        require(assetAddress == msg.sender, "Caller is not a burner or owner");
+        _burn(tokenId);
+    }
+}
