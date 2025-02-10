d42c36b02d8136ce958ff911bb9288e86a624b37
Peter <kompaszbence@gmail.com>
2025-02-10 02:44:42 +0200

    chore: deployment

--- fhevm-hardhat-template/deploy/deploy.ts
+++ fhevm-hardhat-template/deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployed = await deploy("MyConfidentialERC20", {
+  const deployed = await deploy("AuctionFactory", {
     from: deployer,
-    args: ["Naraggara", "NARA"],
+    args: [],
     log: true,
   });
 
-  console.log(`MyConfidentialERC20 contract: `, deployed.address);
+  console.log(`Factory contract: `, deployed.address);
 };
 export default func;
 func.id = "deploy_confidentialERC20"; // id required to prevent reexecution
